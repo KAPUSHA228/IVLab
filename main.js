68244b25f800405153e8cf2ae1d9d048a22f3072
KAPUSHA228 <shura_tsatsyn_2003@mail.ru>
2024-05-23 21:11:17 +0300

    First init

---
+++ main.js
@@ -0,0 +1,11 @@
+// // Получаем ссылку на HTML-элемент, в котором будет отображаться случайное значение
+// const displayElement = document.getElementById('randomValue');
+
+// // Функция для обновления случайного значения в указанном элементе
+// function updateRandomValue() {
+//     const randomNum = Math.floor(Math.random() * 100); // Генерируем случайное число от 0 до 99
+//     displayElement.textContent = randomNum; // Обновляем текст в HTML-элементе
+// }
+
+// // Вызываем функцию обновления случайного значения каждую секунду с помощью setInterval
+// setInterval(updateRandomValue, 2000);
