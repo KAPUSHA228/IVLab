2167f74097735b125658497282f253c997223b64
KAPUSHA228 <shura_tsatsyn_2003@mail.ru>
2024-05-24 20:10:25 +0300

    minupdate

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
-// // Получаем ссылку на HTML-элемент, в котором будет отображаться случайное значение
-// const displayElement = document.getElementById('randomValue');
+const switcher = 1;
 
-// // Функция для обновления случайного значения в указанном элементе
-// function updateRandomValue() {
-//     const randomNum = Math.floor(Math.random() * 100); // Генерируем случайное число от 0 до 99
-//     displayElement.textContent = randomNum; // Обновляем текст в HTML-элементе
-// }
-
-// // Вызываем функцию обновления случайного значения каждую секунду с помощью setInterval
-// setInterval(updateRandomValue, 2000);
+// Функция для обновления случайного значения в указанном элементе
+function updateRandomValue() {
+     const randomNum = Math.floor(Math.random() * 100); // Генерируем случайное число от 0 до 99
+     document.getElementById('randomValue').textContent = randomNum; // Обновляем текст в HTML-элементе
+}
+function switchColor(){
+    document.getElementById('button1').classList.toggle('red');
+    switcher = (switcher===1)?0:1;
+}
+// Вызываем функцию обновления случайного значения каждую секунду с помощью setInterval
+setInterval(updateRandomValue, 2000);
+setInterval(switchColor, 1000);
