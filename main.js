1dbe6d68d5a68160d5aa87a15af13617b980a696
KAPUSHA228 <shura_tsatsyn_2003@mail.ru>
2024-05-26 17:04:54 +0300

    minupdate

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 class Sensor{
-    constructor(number, logs,isOn){
+    constructor(number,logs,isOn){
         this.div=document.querySelector('#sensors');
         this.divLED=document.createElement('div');
         this.divInfo=document.createElement('div');
-        this.divInfo.id='newEl';
+        this.menu=document.createElement('div');
+        this.divInfo.id='inf';
         this.switcher = 1;
         this.number=number;
         this.logs=logs;
@@ -13,36 +14,58 @@ class Sensor{
         this.render();
         setInterval(()=>this.updateRandomValue(),2000);
         setInterval(()=>this.switchColor(), 1000);
+        setInterval(()=>this.toSwitch(),4000);
     }
     toSwitch(){
-        this.isOn=(this.isOn===false)?true:false;
+        this.isOn=!this.isOn;
+        this.divLED.classList.toggle('white');
     }
     updateRandomValue() {
        if(this.isOn===true){
             this.logs = Math.floor(Math.random() * 100); 
-            this.divInfo.textContent= this.logs;}
+            this.divInfo.textContent= this.logs;}     
     }
     switchColor(){
         if(this.isOn===true){
         this.divLED.classList.toggle('red');
         this.switcher = (this.switcher===1)?0:1;}
+        else{
+            this.divLED.classList.add('white');
+        }
     }
     render(){
         this.divInfo.textContent= this.logs;
         this.div.append(this.divLED);
         this.div.append(this.divInfo);
+        this.div.append(this.menu);
+        this.menu.style.display='none';
         this.div.style.width='100%';
         this.div.style.height='200px';
-        
         console.log(this.div);
     }
 }
 const person = new Sensor(1,0,true);
+const person2= new Sensor (1,0,false);
 console.log(person);
 const el=document.querySelector('#sensors');
+const el2=document.querySelectorAll('#inf');
+console.log(el2);
 el.addEventListener('mouseover',function(event){
     el.style.backgroundColor='rgb(127, 180, 70)';
 });
 el.addEventListener('mouseleave',function(event){
     el.style.backgroundColor='steelblue';
+});
+el2.forEach(element=>{
+    element.addEventListener('mouseover',function(){
+        const menu = document.createElement('div');
+        menu.id='menushka';
+        element.insertAdjacentElement('afterend',menu);
+    });
+});
+el2.forEach(element => {
+    element.addEventListener('mouseleave', function () {
+        document.querySelectorAll('#menushka').forEach(local=>{
+            local.remove();});
+    });
 });
