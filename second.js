a53d6adcf60e0edba30d2e5b76a90af01bd9e0ff
KAPUSHA228 <shura_tsatsyn_2003@mail.ru>
2024-05-31 15:58:50 +0300

    minupdate

--- second.js
+++ second.js
@@ -1,44 +1,58 @@
 let div1 = document.createElement('div');
 div1.style.top = '440px';
-div1.style.left = '360px';
+div1.style.left = '760px';
 document.querySelector('body').append(div1);
 let div2 = document.createElement('div');
-div2.style.top = '50%';
-div2.style.left = '69%';
+div2.style.top = '320px';
+div2.style.left = '1040px';
 document.querySelector('body').append(div2);
 let div3 = document.createElement('div');
-div3.style.top = '50%';
-div3.style.left = '78%';
+div3.style.top = '320px';
+div3.style.left = '1180px';
 document.querySelector('body').append(div3);
 let div4 = document.createElement('div');
-div4.style.top = '70%';
-div4.style.left = '68%';
+div4.style.top = '460px';
+div4.style.left = '1040px';
 document.querySelector('body').append(div4);
 let div5 = document.createElement('div');
-div5.style.top = '70%';
-div5.style.left = '91.5%';
+div5.style.top = '400px';
+div5.style.left = '1220px';
 document.querySelector('body').append(div5);
 let div6 = document.createElement('div');
-div6.style.top = '80%';
-div6.style.left = '88%';
+div6.style.top = '380px';
+div6.style.left = '1270px';
 document.querySelector('body').append(div6);
 let div7 = document.createElement('div');
-div7.style.top = '60%';
-div7.style.left = '84%';
+div7.style.top = '400px';
+div7.style.left = '1320px';
 document.querySelector('body').append(div7);
 let div8 = document.createElement('div');
-div8.style.top = '60%';
-div8.style.left = '80%';
+div8.style.top = '460px';
+div8.style.left = '1380px';
 document.querySelector('body').append(div8);
 let div9 = document.createElement('div');
-div9.style.top = '60%';
-div9.style.left = '88%';
+div9.style.top = '540px';
+div9.style.left = '1340px';
 document.querySelector('body').append(div9);
 document.querySelectorAll('div').forEach(element => {
     element.onclick = function () {
         window.location.href = './third.html';
     };
 });
+for (let grid = 89; grid < window.innerHeight-20; grid+=100) {
+    const klmn=document.createElement('section');
+    klmn.id='sec2';
+    klmn.style.top=grid+'px';
+    klmn.style.left = '9px';
+    document.querySelector('body').append(klmn);
+}
+for (let grid = 289; grid < window.innerWidth - 20; grid += 100) {
+    const klmn = document.createElement('section');
+    klmn.id = 'sec2';
+    klmn.style.top = '9px';
+        klmn.style.left = grid + 'px'; 
+    document.querySelector('body').append(klmn);
+}
 function setup() {
     createCanvas(1470, 700); // Создание Canvas
      }
